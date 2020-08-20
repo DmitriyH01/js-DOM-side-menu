17a17611146ecc44e141c881a017c9994d0ff28c
DIMA <t0perxxl@gmail.com>
2020-08-20 11:35:27 +0300

    Added some fixe to visuality

--- code.js
+++ code.js
@@ -2,27 +2,27 @@ const contentMenu = [
   {
     nameClassFromArr: 'menu-item-1',
     contentGoods:
-      '<img class="img-content" src=https://www.logobank.ru/images/ph/en/a2/audi_new.png> <p> Audi в нынешнем виде является наследником концерна Auto Union, образованного в результате объединения четырёх компаний, выпускавших автомобили и мотоциклы под марками DKW, Horch, Audi и Wanderer. </p><p>Всем известно, что главными конкурентами Audi на автомобильном рынке являются компании Mercedes-Benz и BMW, так называемая «Немецкая Тройка». </p>',
+      '<img class="img-content" src=/img/audi.png> <p> Audi в нынешнем виде является наследником концерна Auto Union, образованного в результате объединения четырёх компаний, выпускавших автомобили и мотоциклы под марками DKW, Horch, Audi и Wanderer. </p><p>Всем известно, что главными конкурентами Audi на автомобильном рынке являются компании Mercedes-Benz и BMW, так называемая «Немецкая Тройка». </p>',
   },
   {
     nameClassFromArr: 'menu-item-2',
     contentGoods:
-      '<img class="img-content" src=https://ic.pics.livejournal.com/arbuzov/7078294/434172/434172_original.jpg><p> Является одним из самых узнаваемых автомобильных брендов во всём мире[5]. Штаб-квартира Mercedes-Benz находится в Штутгарте, Баден-Вюртемберг, Германия. </p><p>В 2018 году бренд Mercedes-Benz оценивался в 48,601 млрд долларов, удерживая второе место (после Toyota) среди компаний-производителей автомобилей и восьмое место среди всех брендов мира.</p>',
+      '<img class="img-content" src=/img/mersedes.jpg><p> Является одним из самых узнаваемых автомобильных брендов во всём мире. Штаб-квартира Mercedes-Benz находится в Штутгарте, Баден-Вюртемберг, Германия. </p><p>В 2018 году бренд Mercedes-Benz оценивался в 48,601 млрд долларов, удерживая второе место (после Toyota) среди компаний-производителей автомобилей и восьмое место среди всех брендов мира.</p>',
   },
   {
     nameClassFromArr: 'menu-item-3',
     contentGoods:
-      '<img class="img-content" src=http://aboutcars-ac.ru/wp-content/uploads/2013/02/BMW.png><p> Девиз компании — «Freude am Fahren», с нем. — «С удовольствием за рулём». Для англоязычных стран был придуман также девиз «The Ultimate Driving Machine» (с англ. — «Идеальная машина для вождения»). </p><p>Основные производственные мощности компании сосредоточены в Германии (Дингольфинг, Регенсбург, Лейпциг, Мюнхен).</p>',
+      '<img class="img-content" src=/img/BMW.png><p> Девиз компании — «Freude am Fahren», с нем. — «С удовольствием за рулём». Для англоязычных стран был придуман также девиз «The Ultimate Driving Machine» (с англ. — «Идеальная машина для вождения»). </p><p>Основные производственные мощности компании сосредоточены в Германии (Дингольфинг, Регенсбург, Лейпциг, Мюнхен).</p>',
   },
   {
     nameClassFromArr: 'menu-item-4',
     contentGoods:
-      '<img class="img-content" src=https://autochrome.ru/img/marka-mashiny-s-konem_62.jpg><p> Nissan Motor Co., Ltd.  — японский автопроизводитель, один из крупнейших в мире. Компания основана в 1933 году.</p><p>Эмблема выглядит как название марки в кругу, который означает восходящее солнце, символизирующее искренность.</p>',
+      '<img class="img-content" src=/img/nisan.jpg><p> Nissan Motor Co., Ltd.  — японский автопроизводитель, один из крупнейших в мире. Компания основана в 1933 году.</p><p>Эмблема выглядит как название марки в кругу, который означает восходящее солнце, символизирующее искренность.</p>',
   },
   {
     nameClassFromArr: 'menu-item-5',
     contentGoods:
-      '<img class="img-content" src=https://i1.wp.com/autogeek.com.ua/wp-content/uploads/2017/01/3f39410d0e6da7cacc8b6f4260bfd6d4-387x480.jpg?resize=387%2C480><p> Tesla (ранее Tesla Motors) — американская компания, производитель электромобилей и (через свой филиал SolarCity) решений для хранения электрической энергии.</p><p>Tesla разворачивает сеть «Суперзарядок» (англ. Supercharger) — станций для зарядки электромобилей, разработанных для того, чтобы на автомобилях Tesla можно было совершать длительные поездки. </p>',
+      '<img class="img-content" src=/img/tesla.jpg><p> Tesla (ранее Tesla Motors) — американская компания, производитель электромобилей и (через свой филиал SolarCity) решений для хранения электрической энергии.</p><p>Tesla разворачивает сеть «Суперзарядок» (англ. Supercharger) — станций для зарядки электромобилей, разработанных для того, чтобы на автомобилях Tesla можно было совершать длительные поездки. </p>',
   },
 ];
 
@@ -30,7 +30,7 @@ document.querySelector('.menu-list').addEventListener('click', function (e) {
   //Remove unnecessary сolor from an element
   let nav = document.querySelector('.menu-list').children;
   for (let i = 0; i < nav.length; i++) {
-    if (nav[i].classList.contains('selected-li') === true) {
+    if (nav[i].classList.contains('selected-li') === true && e.target.nodeName === 'LI') {
       nav[i].classList.remove('selected-li');
     }
   }
